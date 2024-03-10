5d66793596d2c3dd9b07b65aa753c9d8d154f541
thoomaas23 <thomas.lazar02@e-uvt.ro>
2024-03-09 17:06:29 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,10 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
-
-
-const char* ssid = "HGGuest";
-const char* password = "haufegroup!";
+const char* ssid = "Razvan - Rabb";
+const char* password = "0809852001";
 const String serverAdress = "https://parking-api.razvan.network/spot/";
-const char* uuid = "cafc63fa-be08-4a0e-9ad4-c37d98bf3c04" ; 
-
+const String uuid = "cafc63fa-be08-4a0e-9ad4-c37d98bf3c04" ; 
 const int trigPin1 = 17;
 const int echoPin1 = 16;
 const int trigPin2 = 2 ;
@@ -15,43 +12,42 @@ const int echoPin2 = 4 ;
 const int ledBlue = 15;
 const int ledRed = 1 ;
 const int ledYellow = 21 ; 
-bool stare ;
+String stare ;
 #define SOUND_SPEED 0.034
 #define CM_TO_INCH 0.393701
-
 long duration1;
 float distanceCm1;
 float distanceCm2 ; 
 long duration2 ;
 void setup() {
-  Serial.begin(115200); 
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
   pinMode(trigPin1, OUTPUT); 
   pinMode(echoPin1, INPUT); 
   pinMode(trigPin2, OUTPUT); 
   pinMode(echoPin2, INPUT); 
-  pinMode (ledBlue, OUTPUT);
-  pinMode (ledRed, OUTPUT);
-  pinMode (ledYellow, OUTPUT);
-  WiFi.begin(ssid, password);
+  int retries = 0;
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
+    delay(500);
+    Serial.print(".");
+    retries++;
+    if (retries > 10) {
+      Serial.println("Failed to connect to WiFi");
+      return;
+    }
   }
   Serial.println("Connected to WiFi");
-  JsonObject requestBody;
-  requestBody["uuid"] = uuid;
+  String body = "{\"uuid\": \""+ uuid + "\"}";
   String data = serverAdress  + "create";
-  if (makePostRequest(data, requestBody)) {
+  if (makePostRequest(data, body)) {
     Serial.println("POST request successful");
   } else {
     Serial.println("POST request failed");
   }
-
 }
 
 void loop() {
   
-  digitalWrite (ledBlue, HIGH);	
   digitalWrite(trigPin1, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin1, HIGH);
@@ -66,43 +62,31 @@ void loop() {
   digitalWrite(trigPin2, LOW);
   duration2 = pulseIn(echoPin2, HIGH);
   distanceCm2 = duration2 * SOUND_SPEED/2;
-  digitalWrite (ledBlue, LOW);	
+  delay(1000);	
+  delay(500);
   if (distanceCm2 < 30 && distanceCm1 < 30 )
-  stare = 0  ;
+    stare = "false" ;
   else
-    stare = 1 ;
- JsonObject patchBody;
- patchBody["uuid"] = uuid;
- patchBody["battery"] = 100;
- patchBody["freeSpot"] = stare ;
+    stare = "true";
+
+ Serial.println(distanceCm1); 
+ Serial.println(distanceCm2);
+ String patchBody = "{\"uuid\": \"" + uuid + "\", \"battery\": 100, \"freeSpot\": " + stare+ "}";
+ Serial.println(patchBody);
  String data = serverAdress  + "notify";
   if (makePatchRequest(data, patchBody)) {
     Serial.println("PATCH request successful");
   } else {
     Serial.println("PATCH request failed");
   }
-delay(200000);
+delay(10000);
 }
-
-bool makePostRequest(const String url, JsonObject& body) {
+bool makePostRequest(const String url, String body) {
   HTTPClient http;
-
-  // Begin HTTP POST request
   http.begin(url);
-
-  // Add content type header
   http.addHeader("Content-Type", "application/json");
-
-  // Serialize JSON to string
-  String jsonString;
-  serializeJson(body,jsonString );
-  // Make the request with the serialized JSON as the body
-  int httpResponseCode = http.POST(jsonString);
-
-  // End HTTP POST request
+  int httpResponseCode = http.POST(body);
   http.end();
-
-  // Check response code
   if (httpResponseCode == HTTP_CODE_OK) {
     return true;
   } else {
@@ -111,26 +95,12 @@ bool makePostRequest(const String url, JsonObject& body) {
   }
 }
 
-bool makePatchRequest(const String url, JsonObject& body) {
+bool makePatchRequest(const String url, String body) {
   HTTPClient http;
-
-  // Begin HTTP PATCH request
   http.begin(url);
-
-  // Add content type header
   http.addHeader("Content-Type", "application/json");
-
-  // Serialize JSON to string
-  String jsonString;
-  serializeJson(body,jsonString );
-
-  // Make the request with the serialized JSON as the body
-  int httpResponseCode = http.PATCH(jsonString);
-
-  // End HTTP PATCH request
+  int httpResponseCode = http.PATCH(body);
   http.end();
-
-  // Check response code
   if (httpResponseCode == HTTP_CODE_OK) {
     return true;
   } else {
